dd5a0e61c671076ac86aba01c089f3bb968a0d40
Jeff Kealy <jeffkealy@MacBook-Pro-2.local>
2016-11-14 15:50:43 -0600

    fist commit

--- server/app.js
+++ server/app.js
@@ -16,3 +16,44 @@ var jokes = [
     punchLine: "If you pee on them they disappear."
   }
 ];
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var port = 8000;
+var path = require('path');
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+
+app.get('/', function(req, res){
+  res.sendFile(path.resolve('./server/public/views/index.html'));
+});
+
+app.get('/jokes', function(req, res){
+  console.log(req.body);
+  console.log("ON server");
+
+  res.send(jokes);
+})
+
+app.post('/jokes', function(req, res){
+  console.log(req.body);
+  jokesReq = req.body
+  jokes.push(jokesReq)
+  res.sendStatus(201);
+})
+
+app.use(express.static('server'));
+
+
+
+app.set('port', process.env.PORT || 8000);
+
+//port
+app.listen(app.get('port'),function(){
+console.log("Listening on port: ", port);
+})
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,47 @@
-console.log('js loaded');
+$(document).ready(function(){
+  console.log('js loaded');
+  getJokes()
+  $('#button').on('click', buttonClick);
+  var jokesObject = {}
+  function getJokes(){
+    $.ajax({
+      type: 'GET',
+      url: '/jokes',
+      success: function(objectFromServer){
+        $('#jokesContainer').empty();
+        $('.input').val('')
+        console.log("back from server");
+        jokesObject = objectFromServer;
+        for (var i = 0; i < objectFromServer.length; i++) {
+        $('#jokesContainer').append('<div class="jokeDiv">Question: ' + objectFromServer[i].jokeQuestion + '</div>');
+        $('#jokesContainer').append('<div class="jokeDiv">Punchline: ' + objectFromServer[i].punchLine + '</div>');
+        $('#jokesContainer').append('<div class="jokeDiv">Author: ' + objectFromServer[i].whoseJoke + '</div>');
+        }
+        console.log(jokesObject);
+      }
+    })
+  }
+  function buttonClick(){
+    var jokeEntered = $('#jokeEnter').val();
+    var punchLineEntered = $('#punchLineEnter').val();
+    var nameEntered = $('#nameEnter').val();
+    var enteredObject = {
+      jokeQuestion: jokeEntered,
+      punchLine: punchLineEntered,
+      whoseJoke: nameEntered
+    }
+    console.log(enteredObject);
+
+      $.ajax({
+        type: 'POST',
+        url: '/jokes',
+        data: enteredObject,
+        success: function(returnedFromPost){
+          console.log(returnedFromPost);
+        }
+      })
+      getJokes();
+
+  }
+
+});
